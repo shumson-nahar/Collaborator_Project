ef86bd42c77866fb27674f38cd6839429d4fd5ec
Shumson-nahar <shumson805@gmail.com>
2023-04-01 21:00:29 -0400

    My First Collaborate project

---
+++ src/main/java/understanding_loop/For_Loop.java
@@ -0,0 +1,10 @@
+package understanding_loop;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/collaborator/COLLABORATOR_PROJECT/AppTest.java
@@ -0,0 +1,20 @@
+package com.collaborator.COLLABORATOR_PROJECT;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
